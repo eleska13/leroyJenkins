2efaeff4c3fad895f21047a871867acd7e8de945
Lsk <francois.keo0@gmail.com>
2018-12-07 15:05:31 +0100

    c2

---
+++ p1.java
@@ -0,0 +1,12 @@
+public class p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Boucle b = new Boucle(1);
+//		b.boucle10();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("i = " + i);
+		}
+
+	}
+}
